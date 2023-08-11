8143bba47fd3cc777c08dde6781088647d62dd56
de <derek@localhost.localdomain>
2023-08-11 11:47:53 +0300

    hot-fix

--- Day1/spin_words.py
+++ Day1/spin_words.py
@@ -3,9 +3,7 @@ def spin_words(sentence):
     #   Reverse each word in word whose len()>5
     #   Join the words in a list words back into a sentence using .join()
     #   Return the joined reversed sentence
-    
-    words = sentence.split()    
-    reversed_words = [word if len(word)<5 else word[::-1] for word in words] 
-    reversed_sentence = " ".join(reversed_words)
-    
-    return reversed_sentence
+  
+  return " ".join([word if len(word)<5 else word[::-1] for word in sentence.split()])
+
+print(spin_words('Convert the sentence into a list'))
